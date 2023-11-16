c12a4612dd1b4479c88864a331f4c8a8e429b66c
John Grabsky <john@arcatar.com>
2023-11-15 19:29:54 -0600

    22 Thymeleaf Templates

--- src/main/java/com/jgrabsky/springwebapp/domain/Publisher.java
+++ src/main/java/com/jgrabsky/springwebapp/domain/Publisher.java
@@ -100,7 +100,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
